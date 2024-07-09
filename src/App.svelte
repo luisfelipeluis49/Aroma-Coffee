e43099af38761baa8c264fd0afd1ad909ff7863d
Luís <luisfelipeluis49@gmail.com>
2024-07-08 21:04:21 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,164 @@
+<script>
+    import { backIn, backOut, circIn, circInOut, circOut, cubicInOut, quadIn, quartIn, quintIn, quintOut, sineIn } from "svelte/easing";
+
+
+    // -- IMPORTS
+
+    import Header from "./components/Header.svelte";
+    import { fade, fly, blur, scale, draw, crossfade, slide } from "svelte/transition";
+
+    // -- VARIABLES
+
+    let drink = 'coffee';
+
+    // -- FUNCTIONS
+
+    function rotate()
+    {
+        drink = drink === 'coffee' ? 'milk' : 'coffee';
+    }
+</script>
+
+<style lang="stylus">
+    // -- STYLES
+
+    article
+    {
+        margin: 0;
+        width: 100%;
+        padding: 0;
+
+        display: flex;
+        flex-direction: column;
+
+        background-color: #EAD4C3;
+
+        font-family: Arial, sans-serif;
+    }
+
+    article.milk
+    {
+        background-color: #FEF9ED;
+    }
+
+    main
+    {
+        width: 100%;
+        height: 93vh;
+        margin-top: 7vh;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .drink
+    {
+        width: 100%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        .can
+        {
+            position: relative;
+
+            height: 25rem;
+            width: 20rem;
+
+            background-image: url("images/can.svg");
+            background-repeat: no-repeat;
+            background-position: center;
+
+            img
+            {
+                position: absolute;
+
+                height: 100%;
+                width: 100%;
+            }
+
+            img.content
+            {
+                padding: 3.5rem 0 0;
+                height: 70%;
+            }
+        }
+
+        button
+        {
+            margin-top: 2rem;
+
+            padding: 1rem 2rem;
+
+            font-size: 1.5rem;
+            color: #333;
+
+            background-color: transparent;
+
+            border: 2px solid #333;
+            border-radius: 1rem;
+
+            cursor: pointer;
+        }
+    }
+
+    h1
+    {
+        position: fixed;
+
+        font-size: 30vw;
+        color: #8D5B33;
+    }
+
+    h1.milk
+    {
+        color: #363234;
+    }
+</style>
+
+<article class:milk={ drink === 'milk' }>
+    <Header />
+    <main>
+        <section class="drink">
+            { #if drink === 'coffee' }
+                <h1 transition:fly={ { delay: 0, duration: 1000, x: -50, y: 0, opacity: 0, easing: circInOut } }>Café</h1>
+            { :else }
+                <h1 class="milk" transition:fly={ { delay: 0, duration: 1000, x: 50, y: 0, opacity: 0, easing: circInOut } }>Leite</h1>
+            { /if }
+            <div class="can">
+                { #if drink === 'coffee' }
+                    <img
+                        class="content"
+                        src="images/coffee_can_content.svg"
+                        alt="Café can content"
+                        transition:fly={ { delay: 0, duration: 1000, x: -50, y: 0, opacity: 0, easing: circInOut } }
+                    />
+                    <img
+                        src="images/coffee_can_outside.svg"
+                        alt="Café can outside"
+                        transition:fly={ { delay: 0, duration: 1000, x: -50, y: 0, opacity: 0, easing: circInOut } }
+                    />
+                { :else }
+                    <img
+                        class="content"
+                        src="images/milk_can_content.svg"
+                        alt="Leite can content"
+                        transition:fly={ { delay: 0, duration: 1000, x: 50, y: 0, opacity: 0, easing: circInOut } }
+                    />
+                    <img
+                        src="images/milk_can_outside.svg"
+                        alt="Leite can outside"
+                        transition:fly={ { delay: 0, duration: 1000, x: 50, y: 0, opacity: 0, easing: circInOut } }
+                    />
+                { /if }
+            </div>
+            <button on:click={ rotate }>
+                Slide
+            </button>
+        </section>
+    </main>
+</article>
---
+++ src/components/Header.svelte
@@ -0,0 +1,89 @@
+<script>
+    // -- IMPORTS
+
+    import Fa from "svelte-fa";
+    import { faBell, faCircleQuestion, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+</script>
+
+<style lang="stylus">
+    header
+    {
+        position: fixed;
+        top: 0;
+
+        height: 7vh;
+        width: 98vw;
+        padding: 0 1vw;
+
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        text-align: center;
+        color: white;
+
+        button
+        {
+            background-color: transparent;
+            border: none;
+        }
+    }
+
+    .toolbar
+    {
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        align-items: center;
+    }
+
+    .search
+    {
+        padding: 0.5rem;
+        border: 1px solid #333;
+        border-radius: 0.5rem;
+
+        display: flex;
+        flex-direction: row;
+        gap: 0.5rem;
+        align-items: center;
+
+        input
+        {
+            border: none;
+            
+            background-color: transparent;
+
+            color: #333;
+        }
+    }
+
+    .divider
+    {
+        height: 2rem;
+        border-left: 1px solid #333;
+    }
+
+    .profile-image
+    {
+        width: 2rem;
+        border-radius: 50%;
+    }
+</style>
+
+<header>
+    <button>
+        <img src="images/logo.svg" alt="">
+    </button>
+    <div class="toolbar">
+        <div class="search">
+            <Fa icon={ faMagnifyingGlass } color="#333"/>
+            <input type="text">
+        </div>
+        <Fa icon={ faCircleQuestion } color="#333"/>
+        <div class="divider" />
+        <Fa icon={ faBell } color="#333"/>
+        <img src="https://picsum.photos/200" alt="" class="profile-image">
+    </div>
+</header>
+
